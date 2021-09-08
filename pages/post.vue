83849c36ca344f0dd760be2f5362f8adcedfdea8
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2021-09-09 00:49:01 +0900

    post.vueでタスクの追加

--- pages/post.vue
+++ pages/post.vue
@@ -5,14 +5,21 @@
       <div class="text-xl">保存</div>
     </div>
     <div class="content px-3 mt-16 mb-96 box-border flex flex-col">
-      <div><input type="text" /></div>
-      <div><input type="text" /></div>
+      <div><input type="text" placeholder="タイトル" v-model="task.title"/></div>
+      <div>
+         <Datetime
+          v-model="task.deadline"
+          :minute-interval="5"
+          :format="'YYYY-MM-DD HH:mm'"
+          :overlay="true"
+        ></Datetime>
+      </div>
       <ul>
-        <li><input type="text" /></li>
+        <li><input type="text" placeholder="タグ" v-model="task.tag"/></li>
       </ul>
       <div><i class="bi bi-plus-circle"></i></div>
-      <div><textarea name="" id="" cols="30" rows="10"></textarea></div>
-      <ul><li><input type="checkbox" /><input type="text" /></li></ul>
+      <div><textarea name="" id="" cols="30" rows="10" placeholder="詳細" v-model="task.description"></textarea></div>
+      <ul><li><input type="checkbox" /><input type="text" v-model="task.todo"/></li></ul>
       <div><i class="bi bi-plus-circle"></i></div>
 
 
@@ -22,8 +29,38 @@
 </template>
 
 <script>
+import Datetime from 'vue-ctk-date-time-picker';
+import '@/node_modules/vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
 export default {
-
+  components: {
+    Datetime
+  },
+ data () {
+   return {
+     task: {
+       title: "",
+       tag: "",
+       descripition: "",
+       deadline:""
+     },
+   }
+ },
+ methods: {
+   submit () {
+     let dbTasks = this.$fire.firestore.collection('tasks')
+     dbTasks
+       .add({
+         title: this.task.title,
+         tag:this.task.tag,
+         description: this.task.description,
+         deadline: this.task.deadline
+       })
+       .then(ref => {
+         console.log('Add ID: ', ref.id)
+       })
+   },
+ },
+ 
 }
 </script>
 
