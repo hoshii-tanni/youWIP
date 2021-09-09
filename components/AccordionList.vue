3b0749d7f016fdc869b0016fe8a6d7f995184061
Numakana <numakana6@gmail.com>
2021-09-09 15:52:42 +0900

    conflictを直した

---
+++ components/AccordionList.vue
@@ -0,0 +1,116 @@
+<template>
+  <dl class="accordion flex flex-col-reverse">
+    <dt @click="click" class="title flex justify-end mt-1">
+      <i class="bi bi-chevron-down flex justify-center items-end text-gray-500 text-xl"></i>
+    </dt>
+    <dd style="display:none;" class="inner">
+      <slot />
+    </dd>
+  </dl>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    
+  },
+  methods:{
+    slideUp(el, duration=300) {
+      el.style.height = el.offsetHeight + "px";
+      el.offsetHeight;
+      el.style.transitionProperty = "height, margin, padding";
+      el.style.transitionDuration = duration + "ms";
+      el.style.transitionTimingFunction = "ease";
+      el.style.overflow = "hidden";
+      el.style.height = "0";
+      el.style.paddingTop = "0";
+      el.style.paddingBottom = "0";
+      el.style.marginTop = "0";
+      el.style.marginBottom = "0";
+      setTimeout(() => {
+        el.style.display = "none";
+        el.style.removeProperty("height");
+        el.style.removeProperty("padding-top");
+        el.style.removeProperty("padding-bottom");
+        el.style.removeProperty("margin-top");
+        el.style.removeProperty("margin-bottom");
+        el.style.removeProperty("overflow");
+        el.style.removeProperty("transition-duration");
+        el.style.removeProperty("transition-property");
+        el.style.removeProperty("transition-timing-function");
+        el.classList.remove("is-open");
+      }, duration);
+    },
+
+    slideDown(el, duration = 300) {
+      el.classList.add("is-open");
+      el.style.removeProperty("display");
+      let display = window.getComputedStyle(el).display;
+      if (display === "none") {
+        display = "block";
+      }
+      el.style.display = display;
+      let height = el.offsetHeight;
+      el.style.overflow = "hidden";
+      el.style.height = "0";
+      el.style.paddingTop = "0";
+      el.style.paddingBottom = "0";
+      el.style.marginTop = "0";
+      el.style.marginBottom = "0";
+      el.offsetHeight;
+      el.style.transitionProperty = "height, margin, padding";
+      el.style.transitionDuration = duration + "ms";
+      el.style.transitionTimingFunction = "ease";
+      el.style.height = height + "px";
+      el.style.removeProperty("padding-top");
+      el.style.removeProperty("padding-bottom");
+      el.style.removeProperty("margin-top");
+      el.style.removeProperty("margin-bottom");
+      setTimeout(() => {
+        el.style.removeProperty("height");
+        el.style.removeProperty("overflow");
+        el.style.removeProperty("transition-duration");
+        el.style.removeProperty("transition-property");
+        el.style.removeProperty("transition-timing-function");
+      }, duration);
+    },
+
+    slideToggle(target, el, duration = 300) {
+      if (window.getComputedStyle(el).display === "none") {
+        target.classList.add("open")
+        this.slideDown(el, duration);
+      } else {
+        target.classList.remove("open")
+        return this.slideUp(el, duration);
+      }
+    },
+
+    click(vl) {
+      //console.log((<HTMLElement>vl.target).parentNode)
+      let target = vl.target;
+
+      while(target.nodeName != "DT") {
+        target = target.parentNode;
+      }
+
+      const sibling = target.nextElementSibling;
+      this.slideToggle(target, sibling);
+    },
+
+  },
+})
+</script>
+
+<style lang="scss">
+.accordion {
+  .title {
+    display: flex;
+    padding: 0 2px;
+
+    &.open i {transform:rotate(180deg);}
+  }
+}
+
+</style>
--- pages/post.vue
+++ pages/post.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex flex-col items-center">
-
     <div class="header w-full h-20 z-10">
       <!-- <i class="bi bi-chevron-left green text-2xl"></i> -->
       <img class="img py-2" src="/back_01.png" width="40" />
-      <div class="px-4 py-2 bg-pink text-white rounded">保存</div>
+      <div @click="display()" class="text-gray-500 font-bold">タスクの編集</div>
+      <div @click="submit()" class="px-4 py-2 bg-pink text-white rounded">保存</div>
     </div>
 
     <div class="content max-w-lg w-full px-3 mt-20 mb-96 box-border flex flex-col">
       <div class="py-4">
-        <input class="w-full px-2 py-4 border-white rounded text-lg" type="text" placeholder="タイトル"/>
+        <input class="w-full px-2 py-4 border-white rounded text-lg font-bold" v-model="title" type="text" placeholder="タイトル"/>
       </div>
 
       <div class="py-3 flex items-center">
         <!-- <i class="bi bi-calendar-check text-2xl pr-1"></i> -->
-        <img class="img pr-1" src="/calendar_green_01.png" width="40" />
+        <img class="img right" src="/calendar_green_01.png" width="40" />
+
         <Datetime
           v-model="deadline"
           :label="'締め切り'"
@@ -22,35 +23,35 @@
       </div>
 
       <ul class="py-3">
-        <li class="flex items-center">
+        <li v-for="(n,i) in tags.length" :key="i" class="flex items-center my-2">
           <!-- <i class="bi bi-hash text-2xl pr-1"></i> -->
-          <img class="img pr-1" src="/hashtag_green_01.png" width="40" />
-          <input class="px-3 py-1 rounded" type="text" placeholder=""/>
+          <img class="img right" src="/hashtag_green_01.png" width="30" />
+          <input class="px-3 py-1 rounded" v-model="tags[i]" type="text" placeholder=""/>
           <!-- <i class="bi bi-trash-fill text-xl pl-2 text-gray-400"></i> -->
-          <img class="img pl-1" src="/trash_green_01.png" width="40" />
+          <img @click="removeTag(i)" class="img left" src="/trash_green_01.png" width="30" />
+        </li>
+        <li @click="addTag()" class="my-1 text-gray-400 flex">
+        <!-- <i class="bi bi-plus-circle pl-3 pr-1"></i> -->
+        <img class="img right" src="/plus_green_01.png" width="25"/> タグを追加する
         </li>
       </ul>
-      <div class="pb-4 text-gray-400 flex">
-        <!-- <i class="bi bi-plus-circle pl-3 pr-1"></i> -->
-        <img class="img pr-1" src="/plus_green_01.png" width="30" /> タグを追加する
-      </div>
+
 
       <div class="py-2">
-        <textarea rows="5" class="w-full p-2 rounded" placeholder="概要"></textarea>
+        <textarea rows="5" class="w-full p-2 rounded" v-model="description" placeholder="概要"></textarea>
       </div>
 
       <ul class="py-2">
-        <li class="flex items-center">
-          <input class="text-xl pr-2" type="checkbox"/>
-          <input class="w-full px-2 py-1 rounded" type="text" placeholder="ToDo..."/>
+        <li v-for="(n,i) in todo.length" :key="i" class="flex items-center my-2">
+          <input class="text-xl pr-2" v-model="todo[i].checked" type="checkbox"/>
+          <input class="w-full px-2 py-1 rounded" v-model="todo[i].title" type="text" placeholder="ToDo..."/>
+          <img @click="removeTodo(i)" class="img left" src="/trash_green_01.png" width="40" />
+        </li>
+        <li @click="addTodo()" class="my-1 text-gray-400 flex">
+          <img class="img right" src="/plus_green_01.png" width="25" />
+          ToDoを追加する
         </li>
       </ul>
-      <div class="pb-4 text-gray-400 flex">
-        <!-- <i class="bi bi-plus-circle pl-3 pr-1"></i> -->
-        <img class="img pr-1" src="/plus_green_01.png" width="30" />ToDoを追加する
-      </div>
-
-
     </div>
     
   </div>
@@ -67,11 +68,70 @@ export default {
   components: {
     Datetime
   },
+
   data() {
     return {
-      deadline: ''
+      title: "", // タイトル
+      deadline: "", // 締め切り
+      description: "", // 概要
+      tags: [], // タグ
+      todo: [], // ToDo
     }
   },
+
+  created() {
+    this.addTag();
+    this.addTodo();
+  },
+
+  methods: {
+    display(){ // デバッグ用メソッド
+      alert(this.tags)
+      console.log(this)
+    },
+
+    // タグの追加
+    addTag() {
+      this.tags.push("");
+    },
+
+    // タグを消す。配列のindexを指定する
+    removeTag(idx) {
+      this.tags.splice(idx, 1);
+    },
+
+    // ToDoの追加
+    addTodo(){
+      this.todo.push({
+        checked: false,
+        title: "",
+        deadline: "",
+      })
+    },
+
+    // ToDoを消す。配列のindexを指定する
+    removeTodo(idx) {
+      this.todo.splice(idx, 1);
+    },
+
+    // 保存
+    submit () {
+      let dbTasks = this.$fire.firestore.collection('tasks')
+      dbTasks
+        .add({
+          title: this.title,
+          tags: this.tags,
+          description: this.description,
+          deadline: this.deadline,
+          todo: this.todo,
+        })
+        .then(ref => {
+          console.log('Add ID: ', ref.id)
+        })
+    },
+
+
+  }
 
 }
 </script>
@@ -79,8 +139,8 @@ export default {
 <style lang="scss" scoped>
 .mt-18 { margin-top: 4.5rem; }
 .bg-pink { background-color: #F45B69; }
-.pink {color: #F45B69;}
-.green {color: #3E9E9C}
+.left {margin-left: 10px}
+.right {margin-right: 10px}
 
 
 /* Date-ctk-picker */
@@ -97,10 +157,16 @@ label.field-label { display: none }
   position: fixed;
   top: 0; left: 0;
 
+  width: 100%; height: 3.5rem;
   padding: 0 .75rem;
+  border-bottom: solid 1px #aaa;
+  border-radius: .5rem;
   box-sizing: border-box;
 
-  backdrop-filter: blur(5px);
+  z-index: 10;
+
+  /*backdrop-filter: blur(5px);*/
+  background-color: #F4F3EE;
 
   display: flex;
   justify-content: space-between;
@@ -128,6 +194,5 @@ input[type="checkbox"]:checked::before {
   content: "\f26c";
   color: #99C9C5;
 }
-
 
 </style>
